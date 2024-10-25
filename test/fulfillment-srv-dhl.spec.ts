c63a5175dc8a046d90c2b762c5888a017a510762
Gerald Baulig <gerald.baulig@n-fuse.co>
2024-10-25 13:43:24 +0200

    fix(test): correct expected response code

--- test/fulfillment-srv-dhl.spec.ts
+++ test/fulfillment-srv-dhl.spec.ts
@@ -317,8 +317,8 @@ describe('Testing Fulfillment Service Cluster:', () => {
         this.timeout(60000);
         const response = await fulfillment_client.submit(sample);
         should.equal(
-          response?.operationStatus?.code, 208,
-          'response.operationStatus.code expected to be 208',
+          response?.operationStatus?.code, 207,
+          'response.operationStatus.code expected to be 207',
         );
         should.ok(
           response?.items?.length ?? 0 > 0,
